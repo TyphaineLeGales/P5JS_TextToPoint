3ec68fcff89b95ca052a4105afeb9edf5116f163
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2019-01-30 14:23:14 +0100

    seek method that creates a vector from the object location to the desired position

--- vehicle.js
+++ vehicle.js
@@ -7,15 +7,30 @@ function Vehicle(x, y) {
   this.acc = createVector();
   this.r = 5;
 
+  this.behaviours = function() {
+    var seek = this.seek(this.target);
+    this.applyForce(seek);
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
   this.update = function() {
     this.pos.add(this.vel);
     this.vel.add(this.acc);
+    this.acc.mult(0);
   }
 
   this.show = function() {
     stroke(15,36,250);
     strokeWeight(5);
     point(this.pos.x, this.pos.y);
+  }
+
+//creating a vector that points from the object location to the desired position
+  this.seek = function(target) {
+    var path = p5.Vector.sub(target, this.pos);
   }
 }
 
