5fccdfa8356dd28822647c374af17e585eb26d05
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2019-01-30 13:20:38 +0100

    textToPoint 3D Cubes

--- P5_sketch.js
+++ P5_sketch.js
@@ -9,25 +9,36 @@ function preload(){
 }
 
 function setup (){
-  var canvas = createCanvas(1400, 800);
+  var canvas = createCanvas(1400, 800, WEBGL);
   canvas.parent('canvasContainer');
-  textSize(250);
+  textSize(100);
   textFont(myFont);
-  points = myFont.textToPoints('hello world', 50, 300);
-  points.forEach(function(dot) {
-    var vehicle = new Vehicle(dot.x, dot.y);
-    vehicles.push(vehicle);
-    console.log(vehicles);
-  });
+  points = myFont.textToPoints('hello world', 0, 0);
 
 }
 
 function draw(){
 background(255);
-vehicles.forEach(function(vehicle) {
-  vehicle.update();
-  vehicle.show();
-});
+points.forEach(function(dot) {
+  push();
+  translate(dot.x, dot.y);
+  normalMaterial();
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.03);
+  box(7);
+  pop();
+
+  push();
+  translate(dot.x+3, dot.y+3);
+  normalMaterial();
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.03);
+  box(7);
+  pop();
+
+
+  });
+
 
 }
 
